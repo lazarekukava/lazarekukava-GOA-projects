990dc6b7492a68fb7e3a349c9f214be3dfd766d2
lazarekukava <lazarekukava@icloud.com>
2024-08-12 20:16:42 +0400

    commit

---
+++ level 14/homework/homework.py
@@ -0,0 +1,65 @@
+#1
+for i in range(10):
+ print(i)
+
+
+for i in range(20):
+ print(i)
+
+
+for i in range(10):
+ print('GOA BEST')
+
+#2
+name = input()
+print(name)
+
+for i in range(5):
+ print('hello' + name)
+
+for i in range(20):
+ print(i/2)
+
+for i in range(15):
+ print(i*2)
+
+#3
+#მას თქვენ ეს ჯერ არ ახსენით და ვერ ვაკეთებ
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
