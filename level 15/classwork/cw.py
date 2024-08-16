6c66e57e9eb6ec85d5898cc7db7686eb4b8d62a3
lazarekukava <lazarekukava@icloud.com>
2024-08-16 21:36:44 +0400

    commit

---
+++ level 15/classwork/cw.py
@@ -0,0 +1,147 @@
+#1
+number = 0
+while number < 10:
+    print(number)
+number = number + 1
+
+#2
+number = 20
+while number < 10:
+    print(number)
+number = number - 1
+
+#3
+number = 0
+while number < 5:
+    print('goa')
+number = number + 1
+
+
+
+#4
+secret = ...
+user_gues = int(input())
+
+while user_gues != secret:
+    user_gues = int (input())
+
+print('mached')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ level 15/homework/hw.py
@@ -0,0 +1,118 @@
+
+#1
+number = 10
+while number < 30:
+    print(number)
+number = number + 1
+
+#2
+number = 50
+while number < 10:
+    print(number)
+number = number - 1
+
+#3
+number = 0
+while number < 10:
+    print('goa best')
+number = number + 1
+
+
+
+#4
+secret = 8
+user_gues = int(input())
+
+while user_gues != secret:
+    user_gues = int (input())
+
+print('u guess it')
+
+#5 მას ეს თქვენ არ ახსენიტ გაკვეთილზე
+
+#6ცვერ გავიგე რა არის გასაკეთებელი
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
